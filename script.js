7e0f05623d1f0866192bf1c1116e96962e172911
Lee <leewhiz119@gmail.com>
2022-12-15 16:26:47 -0500

    added ele-div dragabble and containment

--- script.js
+++ script.js
@@ -3,18 +3,16 @@ const addImgBtn = document.querySelector('#add-img')
 const contentEle = document.querySelector('.content-div')
 
 //Add Ele-Div function
-let addEleDiv =function() {
+// addEleDivBtn.addEventListener('click', function() {
+//     const eleDiv =document.createElement('div')
+//     eleDiv.classList.add('ele-div')
+//     contentEle.appendChild(eleDiv)
+//     $(eleDiv).draggable({
+//       containment: "parent"
+//     })
+// })
 
-}
-
-addEleDivBtn.addEventListener('click', function() {
-    const eleDiv =document.createElement('div')
-    eleDiv.classList.add('ele-div')
-    contentEle.appendChild(eleDiv)
-    $(eleDiv).draggable()
-})
-
-
+//Add Image function
 addImgBtn.addEventListener("change", () => {
   let reader = new FileReader();
   reader.readAsDataURL(addImgBtn.files[0]);
@@ -26,3 +24,65 @@ addImgBtn.addEventListener("change", () => {
     $(img).draggable()
   });
 });
+
+//Append Ele-Div Master function
+let createSubmitField=function(type){
+  elementType= type
+  const eleDiv =document.createElement('div')
+  eleDiv.classList.add('ele-div')
+  eleDiv.classList.add('submit-field')
+  const submitField=document.createElement('div')
+  const inputForm=document.createElement('form')
+  const eleTitle=document.createElement('h3')
+  eleTitle.innerText=elementType + ' Content'
+  eleTitle.classList.add('ele-title')
+  eleDiv.appendChild(eleTitle)
+  //Create text-area for input box
+  const textArea=document.createElement('textarea')
+  //Create btn for input box
+  const submitBtn=document.createElement('button')
+  submitBtn.innerText='Submit'
+  inputForm.addEventListener('submit', (e)=>{
+      console.log(elementType)
+      e.preventDefault()
+      const deleteBtn = document.createElement('div')
+      deleteBtn.innerHTML="&#10006;"
+      deleteBtn.classList.add('delete-btn')
+      const eleDiv2 = document.createElement('div')
+      const innerElement = document.createElement(elementType)
+      innerElement.innerHTML =textArea.value
+      eleDiv2.appendChild(deleteBtn)
+      eleDiv2.appendChild(innerElement)
+      eleDiv2.classList.add('ele-div')
+      contentEle.appendChild(eleDiv2)
+      eleDiv.remove()
+      $(eleDiv2).draggable({
+              containment: "parent"
+          })
+      deleteBtn.addEventListener('click', (e) => {
+          let btn=deleteBtn 
+          $(btn).parent().remove()
+      }) 
+  })
+  submitBtn.classList.add('submit-btn')
+  inputForm.appendChild(textArea)
+ 
+  inputForm.appendChild(submitBtn)
+  eleDiv.appendChild(inputForm)
+  contentEle.appendChild(eleDiv)
+}
+
+
+
+let addTitle=function(){
+  createSubmitField('H1')
+}
+
+let addSubTitle=function(){
+  createSubmitField('H3')   
+}
+
+let addParagraph=function(){
+  createSubmitField('P')
+}
+
